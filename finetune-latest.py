56c0b21caa83dd1911b24821b17dfb2afc09037f
jenniew <jenniewang123@gmail.com>
2023-11-15 12:46:00 -0800

    update

--- finetune-latest.py
+++ finetune-latest.py
@@ -150,9 +150,18 @@ def train(
 
     model = LlamaForCausalLM.from_pretrained(
         base_model,
-        load_in_4bit=True,
+        # load_in_4bit=True,
         torch_dtype=torch.bfloat16,
-        device_map=device_map)
+        device_map=device_map,
+        quantization_config=BitsAndBytesConfig(
+                    load_in_4bit=True,
+                    llm_int8_threshold=6.0,
+                    # llm_int8_has_fp16_weight=False,
+                    # bnb_4bit_compute_dtype=torch.bfloat16,
+                    # bnb_4bit_use_double_quant=True,
+                    bnb_4bit_quant_type="nf4",
+        )
+    )
 
     # model = transformers.AutoModelForCausalLM.from_pretrained(
     #     base_model,
@@ -294,7 +303,8 @@ def train(
             num_train_epochs=num_epochs,
             learning_rate=learning_rate,
             # dataloader_num_workers=16,
-            fp16=True,
+            bf16=True,
+            # fp16=True,
             logging_steps=1,
             optim="adamw_torch",
             evaluation_strategy="steps" if val_set_size > 0 else "no",
