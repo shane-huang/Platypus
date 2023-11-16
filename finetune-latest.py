f6c9ebf4e846835f165bf7f2f751c276bb28bb7a
jenniew <jenniewang123@gmail.com>
2023-11-15 19:14:35 -0800

    update

--- finetune-latest.py
+++ finetune-latest.py
@@ -18,7 +18,7 @@ from peft import (
     set_peft_model_state_dict
 )
 
-from transformers import LlamaForCausalLM, LlamaTokenizer
+from transformers import LlamaForCausalLM, LlamaTokenizer, AutoModelForCausalLM
 
 from utils.prompter import Prompter
 
@@ -148,18 +148,33 @@ def train(
     #     torch_dtype=torch.float16,
     #     device_map=device_map)
 
-    model = LlamaForCausalLM.from_pretrained(
+    # model = LlamaForCausalLM.from_pretrained(
+    #     base_model,
+    #     # load_in_4bit=True,
+    #     torch_dtype=torch.bfloat16,
+    #     device_map=device_map,
+    #     quantization_config=BitsAndBytesConfig(
+    #                 load_in_4bit=True,
+    #                 llm_int8_threshold=6.0,
+    #                 # llm_int8_has_fp16_weight=False,
+    #                 # bnb_4bit_compute_dtype=torch.bfloat16,
+    #                 # bnb_4bit_use_double_quant=True,
+    #                 bnb_4bit_quant_type="nf4",
+    #     )
+    # )
+
+    model = AutoModelForCausalLM.from_pretrained(
         base_model,
         # load_in_4bit=True,
         torch_dtype=torch.bfloat16,
         device_map=device_map,
         quantization_config=BitsAndBytesConfig(
-                    load_in_4bit=True,
-                    llm_int8_threshold=6.0,
-                    # llm_int8_has_fp16_weight=False,
-                    # bnb_4bit_compute_dtype=torch.bfloat16,
-                    # bnb_4bit_use_double_quant=True,
-                    bnb_4bit_quant_type="nf4",
+            load_in_4bit=True,
+            llm_int8_threshold=6.0,
+            # llm_int8_has_fp16_weight=False,
+            # bnb_4bit_compute_dtype=torch.bfloat16,
+            # bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type="nf4",
         )
     )
 
